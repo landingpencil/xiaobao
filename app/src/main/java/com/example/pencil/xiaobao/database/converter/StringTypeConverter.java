3722e24a62da8b57c025d664b018aaae61828f42
landingpencil <landingpencil@gmail.com>
2018-05-29 06:56:57 +0800

    05290656

---
+++ app/src/main/java/com/example/pencil/xiaobao/database/converter/StringTypeConverter.java
@@ -0,0 +1,22 @@
+package com.example.pencil.xiaobao.database.converter;
+
+import android.arch.persistence.room.TypeConverter;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringTypeConverter {
+
+  @TypeConverter
+  public static String stringListToString(List<String> strings) {
+    return new Gson().toJson(strings);
+  }
+
+  @TypeConverter
+  public static List<String> stringToStringList(String stirng) {
+    Type listType = new TypeToken<ArrayList<String>>(){}.getType();
+    return new Gson().fromJson(stirng, listType);
+  }
+}
